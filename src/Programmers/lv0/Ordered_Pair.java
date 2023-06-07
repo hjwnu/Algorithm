a66f6f98eccee4a2b74c3b61504faae8de950bcb
hjwnu <tlstjr01@gmail.com>
2023-06-07 23:45:13 +0900

    lv0. 순서쌍의 개수

--- src/Programmers/lv0/Ordered_Pair.java
+++ src/Programmers/lv0/Ordered_Pair.java
@@ -1,9 +1,17 @@
 package Programmers.lv0;
 
+/*
+문제 설명
+순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 1,000,000
+ */
 public class Ordered_Pair {
     public static void main(String[] args) {
         for(int i =0; i <= 10000;i++){
-            System.out.println(i + " => "+solution(i) + "\n");
+            System.out.println(i + " 기=> "+solution(i) + "\n");
         }
     }
 
