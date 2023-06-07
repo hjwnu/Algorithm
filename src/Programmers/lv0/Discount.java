c22f1213073a02a7d0bc6dcebfd82d73ad429be1
hjwnu <tlstjr01@gmail.com>
2023-06-07 23:29:51 +0900

    lv0. 옷가게 할인받기

---
+++ src/Programmers/lv0/Discount.java
@@ -0,0 +1,27 @@
+package Programmers.lv0;
+
+public class Discount {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 1000010; i+=100000){
+            System.out.println(solution(i));
+        }
+    }
+    public static int solution(int price) {
+        int answer =0 ;
+
+        if(price >= 500000){
+            answer = price*4/5;
+            }
+        else if(price >= 300000){
+            answer = price *9/10;
+            }
+        else if (price >= 100000){
+            answer = price * 19/20 ;
+            }
+        else {
+            answer = price;
+            }
+        return answer;
+    }
+
+}
