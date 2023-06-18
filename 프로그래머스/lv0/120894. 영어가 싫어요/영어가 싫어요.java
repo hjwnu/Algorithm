5ab4a7128ca6286c3363a4645bcfc6f26887d09c
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-06-18 12:37:12 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.32 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,10 +1,9 @@
 class Solution {
     public long solution(String numbers) {
         long answer = 0;
-        String[] num = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine","zero"};
-        String[] n = {"1","2","3","4","5","6","7","8","9","0"};
+        String[] num = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for (int i =0 ; i < num.length;i++){
-            if(numbers.contains(num[i])){ numbers = numbers.replaceAll(num[i],n[i]);}
+            if(numbers.contains(num[i])){ numbers = numbers.replaceAll(num[i],String.valueOf(i));}
         }
         return Long.parseLong(numbers);
     }
