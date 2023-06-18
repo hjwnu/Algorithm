1d8fad9ce670cc32751bd6eab5ec49ecff84a1cb
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-06-18 21:17:57 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.46 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -5,20 +5,17 @@ class Solution {
         int remain = n - k;  // 만들어야 할 결과 숫자의 자릿수
 
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < n; i++) {
             char digit = number.charAt(i);
-
             // 스택에 숫자가 있고, 현재 탐색 중인 숫자보다 작은 숫자가 스택에 있다면 제거
             while (!stack.isEmpty() && stack.peek() < digit && k > 0) {
                 stack.pop();
                 k--;
             }
-            
             stack.push(digit);
         }
-
-         StringBuilder sb = new StringBuilder();
+        
+        StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()) {
             sb.insert(0, stack.pop());
         }
