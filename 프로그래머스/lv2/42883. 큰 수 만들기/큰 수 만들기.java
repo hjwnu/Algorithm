882b5ab1136cf2731e260321ef8a7042cc6c42ed
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-06-19 09:03:34 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.27 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -23,3 +23,35 @@ class Solution {
         return sb.substring(0, remain);
     }
 }
+
+//    public static String solution(String number, int k) {
+//        int len = number.length()-k; // 구해야할 값의 길이
+//        int[] temp = new int[len];
+//        int idx = 0, cnt = 0;
+//
+//        while(cnt<len){
+//            int max= 0;
+//            int maxIdx = -1;
+//            for (int i =idx; i <= k+cnt;i++){
+//                int a = (int)number.charAt(i)-'0';
+//                if( a > max){ max = a; maxIdx = i;}
+//            }
+//            temp[cnt] = max; cnt++;
+//            idx = maxIdx+1;
+//        }
+//
+//        // to String
+//        String ans ="";
+//        for( int i : temp){
+//            ans += String.valueOf(i);
+//        }
+//        return ans;
+//    }
+//}
+//
+//    0     ~ length - (len-1) 중에 최대값 ==> len(구해야할 길이) = length - k /// length - (len - i) = k+i;
+// 고른 인덱스 ~ length - (len-2)
+// 고른 인덱스 ~ length - (len-3)
+// ...
+// 고른 인덱스 ~ length - (len-len) 중 최대값
+//
