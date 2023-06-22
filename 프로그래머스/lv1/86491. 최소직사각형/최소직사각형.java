3accdaf3e3d1890a03da6acabe48726c27554a86
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-06-22 22:45:01 +0900

    [level 1] Title: 최소직사각형, Time: 2.28 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -4,43 +4,16 @@ class Solution {
     public static int solution(int[][] sizes) {
         int[] max = max(sizes);
         for(int i =0; i < sizes.length;i++){
-            // if(max[0]>max[1]){
-                if(sizes[i][0]<sizes[i][1]){
+                if(sizes[i][0]<sizes[i][1]){ // 모든 긴 길이를 가로로 몰빵
                     int temp = sizes[i][0];
                     sizes[i][0] = sizes[i][1];
                     sizes[i][1] = temp;
                 }
-        //     }
-        //     if(max[0]<max[1]){
-        //         if(sizes[i][0]>sizes[i][1]){
-        //             int temp = sizes[i][0];
-        //             sizes[i][0] = sizes[i][1];
-        //             sizes[i][1] = temp;
-        //         }
-        //     }
         }
         max = max(sizes);
         return max[0]*max[1];
     }
 
-    //     int[] max = new int[4];
-    //     while(true) { // 최소가 될때까지 반복.
-    //         max = max(sizes);//바뀌기 전 size =temp
-    //         if (max[0] < max[1]) { // 가로 최댓값이 세로보다 낮다면 바꾸고,
-    //             if(sizes[max[2]][0]<=sizes[max[2]][1]){break;} 
-    //             int temp = sizes[max[2]][0];
-    //             sizes[max[2]][0] = sizes[max[2]][1];
-    //             sizes[max[2]][1] = temp;
-    //         } else if (max[0] > max[1]) { // 세로 최댓값이 가로보다 낮다면 바꾼다.
-    //             if(sizes[max[3]][0]>=sizes[max[3]][1]){break;}
-    //             int temp = sizes[max[3]][0];
-    //             sizes[max[3]][0] = sizes[max[3]][1];
-    //             sizes[max[3]][1] = temp;
-    //         }
-    //     }
-    //     // 가로 세로 맥스를 곱하여 리턴.
-    //     return max[0]*max[1];
-    // }
     public static int[] max(int[][] sizes){
         int[] max = new int[2];
         for(int i= 0; i < sizes.length;i++){
@@ -50,3 +23,15 @@ class Solution {
         return max;
     }
 }
+// 가로, 세로 중 긴것 중에 가장 긴 것 * 가로,세로 중 짧은 것 중에 가장 긴 것...
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int length = 0, height = 0;
+//         for (int[] card : sizes) {
+//             length = Math.max(length, Math.max(card[0], card[1]));
+//             height = Math.max(height, Math.min(card[0], card[1]));
+//         }
+//         int answer = length * height;
+//         return answer;
+//     }
+// }
