bfda2daa4e0c3b51798c3c7ac24099db277b9e23
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-04 18:43:52 +0900

    [level 2] Title: 오픈채팅방, Time: 390.10 ms, Memory: 328 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,16 +3,16 @@ class Solution {
     public String[] solution(String[] record) {
         String[][] split = new String[record.length][3];
         for(int i =0; i < record.length;i++){
-            split[i] = record[i].split(" ");
+            split[i] = record[i].split(" "); // ["Enter","uid1234","Muzi"] 형태로 쪼개기
         }
         
-        Map<String, String> map = new HashMap<>();
-        for(int i=0; i < split.length;i++){
+        Map<String, String> map = new HashMap<>();// Map("uid1234", "Muzi") 형태로 저장
+        for(int i=0; i < split.length;i++){ // 변경된 아이디는 맨 마지막 아이디로 대체하여 저장
             if(split[i].length==3){map.put(split[i][1],split[i][2]);}
         }
         
-        List<String> answer = new ArrayList<>();
-        
+        // 맵에서 아이디(키값)으로 닉네임을 뽑아와 Enter, Leave만 필터링하여 저장
+        List<String> answer = new ArrayList<>(); 
         for(int i =0 ; i < split.length;i++){
             if(split[i][0].equals("Enter")){
                 answer.add(String.format("%s님이 들어왔습니다.", map.get(split[i][1])));
@@ -22,6 +22,7 @@ class Solution {
             }
         }
         
+        // List to Array
         String[] ans = new String[answer.size()];
         for(int i =0; i< answer.size();i++){
             ans[i] = answer.get(i);
