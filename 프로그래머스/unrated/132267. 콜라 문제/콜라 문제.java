8b652a7dcbd0cb6f3e688aa0b045f107d59ed2cf
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-07 18:45:52 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int emptyBottles = n;
         int ans = 0;
 
-        while (emptyBottles >= a) {
-            int takeBack = emptyBottles / a;
-            emptyBottles %= a;
-            emptyBottles += takeBack * b;
-            ans += takeBack * b;
+        while (n/a>0) {
+            ans += (n/a) * b;
+            n = (n/a)*b + n%a;
         }
-
         return ans;
+    }
 }
-  }
