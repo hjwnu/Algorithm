f60fbdf0d63bae92df741ea9de61cc44ce49364f
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-07 15:39:00 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 13.81 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "0";
+        for(int i =food.length-1 ; i >0;i--){
+            if(food[i]>=2){
+                if(food[i]%2==1){
+                    answer = String.valueOf(i).repeat((food[i]-1)/2) + answer + String.valueOf(i).repeat((food[i]-1)/2);
+                }
+                else { answer = String.valueOf(i).repeat(food[i]/2)+ answer +String.valueOf(i).repeat(food[i]/2);}
+            }
+        }
+        return answer;
+    }
+}
