dbe386307d3694af35abb588e075e7e7444fbf46
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-10 13:32:34 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.24 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -10,9 +10,10 @@ class Solution {
                 default:
                     if(numbers[i]==0){numbers[i]=11;}
                     
+                    //현재 눌러야할 키패드로부터 각각 왼손, 오른손 거리를 구하고 무엇을 입력할지 결정.
                     String temp = whichHand(leftD(numbers[i], curLeft), 
-                                        rightD(numbers[i], curRight), 
-                                        hand);
+                                            rightD(numbers[i], curRight), 
+                                            hand);
                     
                     if(temp.equals("L")){curLeft = numbers[i];}
                     else{curRight = numbers[i];}
@@ -24,7 +25,8 @@ class Solution {
     }
     
     public String whichHand(int l, int r, String hand){
-        return l==r? hand.equals("left")? "L":"R" : l>r? "R" : "L";
+        return l==r? (hand.equals("left")? "L":"R") : (l>r? "R" : "L");
+        // 두 거리가 같다면 주 사용 손으로,                  다르다면 거리가 짧은 손으로 입력 
     }
     
     public int leftD(int numbers, int curLeft){
@@ -44,10 +46,10 @@ class Solution {
     public int rightD(int numbers, int curRight){
         int rightDistance = Math.abs(numbers-curRight);
         switch(rightDistance%2){
-            case 1:if(rightDistance>4){rightDistance=3;}
+            case 1:if(rightDistance>3){rightDistance=3;}
                    else if(rightDistance>=1){rightDistance=1;} break;
 
-            case 0:if(rightDistance>7){rightDistance=4;}
+            case 0:if(rightDistance>6){rightDistance=4;}
                    else if(rightDistance>=2){rightDistance=2;} break;
         }
         
