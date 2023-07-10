4227e947ec9bbcb3ea37cca13e2bba659fb6dfb6
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-10 13:13:09 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.59 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        int curLeft = 10; // "*" = 10, "0" = 11, "#" = 12
+        int curRight = 12;
+        for(int i =0 ; i < numbers.length; i++){
+            if(numbers[i]==1||numbers[i]==4||numbers[i]==7){
+                answer += "L"; curLeft = numbers[i];
+            }
+            else if(numbers[i]==3||numbers[i]==6||numbers[i]==9){
+                answer += "R"; curRight = numbers[i];
+            }
+            else{
+                if(numbers[i]==0){numbers[i]=11;}
+                int lDistance = leftD(numbers[i], curLeft);
+                int rDistance = rightD(numbers[i], curRight);
+                if(lDistance>rDistance){answer += "R"; curRight=numbers[i];}
+                else if(lDistance<rDistance){answer += "L"; curLeft=numbers[i];}
+                else{
+                    if(hand.equals("left")){answer += "L"; curLeft=numbers[i];}
+                    else{answer += "R"; curRight=numbers[i];}
+                }
+            }
+        }
+     return answer;
+    }
+    public int leftD(int numbers, int curLeft){
+        int leftDistance = Math.abs(numbers-curLeft);
+        switch(leftDistance%2){
+            case 1:if(leftDistance>3){leftDistance=3;}
+                   else if(leftDistance>=1){leftDistance=1;} break;
+
+            case 0:if(leftDistance>6){leftDistance=4;}
+                   else if(leftDistance>=2){leftDistance=2;} break;
+        }
+        return leftDistance;
+    }    
+    public int rightD(int numbers, int curRight){
+        int rightDistance = Math.abs(numbers-curRight);
+        switch(rightDistance%2){
+            case 1:if(rightDistance>4){rightDistance=3;}
+                   else if(rightDistance>=1){rightDistance=1;} break;
+
+            case 0:if(rightDistance>7){rightDistance=4;}
+                   else if(rightDistance>=2){rightDistance=2;} break;
+        }
+        
+        return rightDistance;
+    }   
+}
