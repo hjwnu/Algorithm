41e9b13fac716bcc119f1987552ede818902f76b
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-14 15:38:01 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.47 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,24 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        Queue<Character> brk = new LinkedList<>();
-    for(char ch: s.toCharArray()){
-        brk.add(ch);
-    }
+    Queue<Character> brk = new LinkedList<>(); // Queue로 전환
+    for(char ch: s.toCharArray()){brk.add(ch);}
 
-    Queue<Character> temp = new LinkedList<>(brk);
-    int ans =0;
+    Queue<Character> temp = new LinkedList<>(brk); // 되돌아왔는지 비교를 위한 큐 생성
+    int ans =0; // 올바른 괄호문자열 여부 체크
 
     do{
-        if(pairBrk(brk)){ans++;}
-        brk.add(brk.poll());
-    }while(!brk.equals(temp));
+        if(pairBrk(brk)){ans++;} // 올바른 괄호문자열이면 +1
+        brk.add(brk.poll()); // 괄호 회전
+    }while(!brk.equals(temp)); // 처음으로 되돌아올때까지 반복
 
     return ans;
 }
     public static boolean pairBrk(Queue<Character> bracket){
-        Queue<Character> clone = new LinkedList<>(bracket);
-        Stack<Character> tmp = new Stack<>();
+        Queue<Character> clone = new LinkedList<>(bracket); // 괄호쌍 체크를 위해 큐 복사
+        Stack<Character> tmp = new Stack<>(); 
         while(!clone.isEmpty()){
             char ch = clone.poll();
             switch(ch){
