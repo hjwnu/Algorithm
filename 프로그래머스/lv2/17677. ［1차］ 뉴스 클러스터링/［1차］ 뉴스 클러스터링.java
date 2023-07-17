37d18661d3e1f9bab89796d080f1ffba452674fe
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-17 10:46:16 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.55 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,12 +1,21 @@
 import java.util.*;
 class Solution {
+    static Map<String,Integer> union = new HashMap<>();
+    static Map<String,Integer> insec = new HashMap<>();
+    
     public int solution(String str1, String str2) {
         Map<String, Integer> first = new HashMap<>();
         Map<String, Integer> second = new HashMap<>();
 
-        savePairLetter(first, str1); savePairLetter(second, str2);
+        savePairLetter(first, str1); savePairLetter(second, str2); // 각 글자쌍 맵에 저장
         
-        return (int)(unionAndIntersection(first,second)*65536);
+        // 합집합과 교집합 구하기. 
+        unionAndIntersec(first, second); //각 메서드로 나누면 동일한 반복문을 여러번 중복해야 함.
+        
+        double unisum = valueSum(union);
+        double insecsum = valueSum(insec);
+        
+        return unisum==0? 65536:(int)(insecsum/unisum*65536);
     }
     
     public Map<String, Integer> savePairLetter(Map<String, Integer> first, 
@@ -14,33 +23,31 @@ class Solution {
         str= str.toUpperCase();
         for (int i = 0; i < str.length() - 1; i++) {
             String pair = str.substring(i, i + 2);
-            if (pair.matches("[A-Z]{2}")) {
-                first.put(pair, first.getOrDefault(pair, 0) + 1);
+            if (pair.matches("[A-Z]{2}")) { // 영 대문자 2글자로만 이루어진 문자열만
+                first.put(pair, first.getOrDefault(pair, 0) + 1); 
             }
         }
         return first;
     }
     
-    public double unionAndIntersection(Map<String, Integer> first, Map<String, Integer> second){
-        Map<String,Integer> union = new HashMap<>();
-        Map<String,Integer> insec = new HashMap<>();
+    public void unionAndIntersec(Map<String, Integer> first, Map<String, Integer> second){
         for(String key : first.keySet()){
+            int value1 = first.get(key);
             if(second.containsKey(key)){
-                union.put(key,Math.max(first.get(key),second.get(key)));
-                insec.put(key,Math.min(first.get(key),second.get(key)));
-            }
-            else {union.put(key, first.get(key));}
+                //양 쪽에 모두 있는 글자라면 합집합에는 최대치를, 교집합은 최소치를 넣어야 한다.
+                int value2 = second.get(key);
+                union.put(key,Math.max(value1, value2)); 
+                insec.put(key,Math.min(value1, value2));
+            } // 한쪽에만 있다면, 따로 추가
+            else {union.put(key, value1);}
         }
+        
+        // 중복되지 않은 두번째 문자열의 글자쌍 추가
         for(String key : second.keySet()){
             if(!union.containsKey(key)){
                 union.put(key, second.get(key));
             }
         }
-        
-        double unisum = valueSum(union);
-        double insecsum = valueSum(insec);
-        
-        return unisum == 0? 1:insecsum/unisum;
     }
     
     public double valueSum(Map<String, Integer> map){
