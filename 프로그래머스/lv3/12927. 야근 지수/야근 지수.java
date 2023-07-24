fb2e8355b006e68e41f91b35530c06773a0d671d
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-25 00:55:45 +0900

    [level 3] Title: 야근 지수, Time: 117.47 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
+        //제곱이 최소화돼야하므로, 남은 작업량 중 Max값을 찾아 줄여야 함.
         Queue<Integer> prior = new PriorityQueue<>(Comparator.reverseOrder());
         for(int i : works){prior.add(i);}
         
