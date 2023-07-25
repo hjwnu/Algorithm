1d7724dc7cd40c455136b0f64365ed825f673892
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-25 16:42:07 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.46 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] fees, String[] records) {
+        Map<String, Integer> parkingTime = new HashMap<>();
+        for(int i= 0; i < records.length;i++){
+            String[] split = records[i].split(" ");
+            String[] time = split[0].split(":");
+            String carNum = split[1];
+            int hour = Integer.parseInt(time[0])*60; int minute = Integer.parseInt(time[1]);
+            if(split[2].equals("IN")){
+                parkingTime.put(carNum,parkingTime.getOrDefault(carNum,0)-hour-minute);
+            }
+            if(split[2].equals("OUT")){
+                parkingTime.put(carNum,parkingTime.getOrDefault(carNum,0)+hour+minute);
+            }
+        }
+        Map<String, Integer> sortCarNum = new TreeMap<>(parkingTime);
+
+        int[] ans = new int[sortCarNum.size()];
+        int idx =0;
+        for(Map.Entry<String,Integer> entry : sortCarNum.entrySet()){
+            int value = entry.getValue();
+            String key = entry.getKey();
+            if(value<=0){
+                sortCarNum.put(key, sortCarNum.getOrDefault(key,0)+23*60+59);
+                value = entry.getValue();}
+
+            if(value<=fees[0]){
+                ans[idx++] = fees[1];
+            }
+            else{
+                double tmpTime = value-fees[0];
+                double tmpFee = fees[2];
+                ans[idx++] = fees[1]+(int)(Math.ceil(tmpTime/tmpFee))*fees[3];
+            }
+        }
+        System.out.println(sortCarNum);
+        return ans;
+    }
+    
+    
+}
