cf2b1b1f88c165410b0890cd294a75071bcb67b8
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-25 16:46:19 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.29 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -8,10 +8,10 @@ class Solution {
             String carNum = split[1];
             int hour = Integer.parseInt(time[0])*60; int minute = Integer.parseInt(time[1]);
             
-            if(split[2].equals("IN")){
+            if(split[2].equals("IN")){ // 입차시간은 모두 뺄셈하고,
                 parkingTime.put(carNum,parkingTime.getOrDefault(carNum,0)-hour-minute);
             }
-            if(split[2].equals("OUT")){
+            if(split[2].equals("OUT")){ // 출차시간은 모두 덧셈하면, 총 주차시간을 알 수 있다.
                 parkingTime.put(carNum,parkingTime.getOrDefault(carNum,0)+hour+minute);
             }
         }
@@ -23,22 +23,18 @@ class Solution {
         for(Map.Entry<String,Integer> entry : sortCarNum.entrySet()){
             int value = entry.getValue();
             String key = entry.getKey();
-            if(value<=0){
-                sortCarNum.put(key, sortCarNum.getOrDefault(key,0)+23*60+59); // 만약 출차X라면, 23:59에 출차한 것으로 간주
+            if(value<=0){ // 만약 출차X라면, 주차시간값은 언제나 음수다. 23:59에 출차한 것으로 간주한다. 
+                sortCarNum.put(key, sortCarNum.getOrDefault(key,0)+23*60+59); 
                 value = entry.getValue();}
-
-            if(value<=fees[0]){
+            
+            if(value<=fees[0]){ // 기본 주차시간 이하
                 ans[idx++] = fees[1]; 
-            }
-            else{
+            } else{
                 double tmpTime = value-fees[0];
                 double tmpFee = fees[2];
                 ans[idx++] = fees[1]+(int)(Math.ceil(tmpTime/tmpFee))*fees[3]; // 단위 시간으로 나눠떨어지지 않으면 올림 계산
             }
         }
-        System.out.println(sortCarNum);
         return ans;
-    }
-    
-    
+    }    
 }
