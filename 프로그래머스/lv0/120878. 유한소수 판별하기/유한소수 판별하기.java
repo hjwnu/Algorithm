d0f2c5b063c3be929b84cc89fcf015e053dbffe3
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-27 02:04:54 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int a, int b) {
+        if(a%b==0) return 1; // 정수는 유한소수
+        
+        int gcd = GCD(a,b); a /= gcd; b /= gcd;
+        
+        if(b%2!=0&&b%5!=0) return 2; // 2와 5가 소인수로 없다면 무한소수
+        
+        for(int i =3; i <= b/2; i++){
+            if(b%i==0&&(i%2!=0&&i%5!=0)){return 2;}  // 2와 5 이외의 소인수가 있다면 무한소수
+        }
+        return 1;
+    }
+    
+    public int GCD(int m,int n){
+        if(m < n){ int tmp = n; n = m; m = tmp;}
+        if(m%n==0) return n;
+        return GCD(n, m%n);
+    }
+}
