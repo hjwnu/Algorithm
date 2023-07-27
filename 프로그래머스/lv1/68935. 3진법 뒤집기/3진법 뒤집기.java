ec990cd3d3f9c3ad48de5aac1b49432d813f79a8
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-27 20:16:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 6.48 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,10 +1,11 @@
+import java.util.*;
 class Solution {
     public long solution(long n) {
-        String a = "";
+        StringBuilder a = new StringBuilder();
         while(n!=0){
-            a += n%3+""; n /= 3;
+            a.append(n%3+""); n /= 3;
         }
-        long b = Long.parseLong(a); long ans =0;
+        long b = Long.parseLong(a.toString()); long ans =0;
         for( long i =0; i < a.length();i++){
             ans += b%10*Math.pow(3,i);  b /= 10; 
         }
