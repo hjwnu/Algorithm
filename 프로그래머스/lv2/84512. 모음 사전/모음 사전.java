9b45ea7d906f8d755bd8da6d3f553b3d3603873f
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-27 15:40:04 +0900

    [level 2] Title: 모음 사전, Time: 38.74 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    
+    static List<String> dict = new ArrayList<>();
+    static String[] vowel = {"A","E","I","O","U"};
+    
+    public int solution(String word) {
+        for(int i =0 ; i < vowel.length;i++){
+            addWord(dict, vowel[i]); // 각 모음으로 시작하는 모든 문자 탐색
+        }    
+        return dict.indexOf(word)+1;
+    }
+    
+    void addWord(List<String> dict, String str){
+        if(str.length() > vowel.length) {return;}
+        
+        if(!dict.contains(str)) dict.add(str);
+    
+        for(int i=0; i<vowel.length; i++){
+            addWord(dict, str+vowel[i]);
+        }
+    }
+}
