55a7054d2076a9c503a64b94014ed32e13382241
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-31 09:34:03 +0900

    [level 2] Title: 방문 길이, Time: 2.77 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -2,17 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(String dirs) {
         Set<String> route = new HashSet<>();
-        int x = 5; int y = 5;
+        int x = 0; int y = 0;
         for(char ch : dirs.toCharArray()){
             StringBuilder beforeM = new StringBuilder();
             StringBuilder afterM = new StringBuilder();
             beforeM.append(x); beforeM.append(y);
             boolean isMoved = true;
             
-            if(ch=='U'&&y<10){y++;}
-                else if(ch=='D'&&y>0){y--;}
-                else if(ch=='R'&&x<10){x++;}
-                else if(ch=='L'&&x>0){x--;}
+            if(ch=='U'&&y<5){y++;}
+                else if(ch=='D'&&y>-5){y--;}
+                else if(ch=='R'&&x<5){x++;}
+                else if(ch=='L'&&x>-5){x--;}
                 else{isMoved=false;}
             
              if(isMoved){
