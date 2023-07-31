cb94095d734d30cc0d28297d5496241de280d47b
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-07-31 10:14:12 +0900

    [level 2] Title: 방문 길이, Time: 1.83 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -1,34 +1,47 @@
 import java.util.*;
 class Solution {
+    int x, y = 0; // 현재 좌표 x,y
+    boolean isMoved;
+    
     public int solution(String dirs) {
         Set<String> route = new HashSet<>(); // 중복 경로 체크를 위한 HashSet
-        int x = 0; int y = 0; // 현재 좌표 x,y
-        
-        for(char ch : dirs.toCharArray()){
-            StringBuilder beforeM = new StringBuilder(); 
-            beforeM.append(x); beforeM.append(y); // 이동 전 좌표
+        for(char dir : dirs.toCharArray()){
+            StringBuilder beforeM = currentXY();
             
-            boolean isMoved = true; // 이동했는지 체크
-            if(ch=='U'&&y<5){y++;}
-            else if(ch=='D'&&y>-5){y--;}
-            else if(ch=='R'&&x<5){x++;}
-            else if(ch=='L'&&x>-5){x--;}
-            else{isMoved=false;}
+            move(dir); 
             
-             if(isMoved){ // 이동했을 경우에만 나머지작업 처리
-                 
-                StringBuilder afterM = new StringBuilder(); // 이동 후 좌표
-                afterM.append(x); afterM.append(y);
+            if(isMoved){ // 이동했을 경우에만 나머지작업 처리             
+                StringBuilder afterM = currentXY();
                 
-                StringBuilder temp = new StringBuilder(afterM); // 동일한 경로를 역방향으로 지났는지 체크
-                temp.append(beforeM);
-                 
-                if(!route.contains(temp.toString())){ 
+                // 동일한 경로를 반대방향으로 지난 적 있는지 체크 후, 역방향으로도 안 지난 경우에만 해시셋에 추가
+                boolean pr = passedReverse(beforeM, afterM, route); 
+                if(!pr){ 
                     beforeM.append(afterM);
                     route.add(beforeM.toString());
                 }
             }
         }
         return route.size();
-        }
+    }
+    
+    void move(char dir){
+        isMoved = true;
+        if(dir=='U'&&y<5){y++;}
+        else if(dir=='D'&&y>-5){y--;}
+        else if(dir=='R'&&x<5){x++;}
+        else if(dir=='L'&&x>-5){x--;}
+        else{isMoved=false;}
+    }
+    
+    StringBuilder currentXY(){
+        StringBuilder M = new StringBuilder(); 
+        M.append(x); M.append(y);
+        return M;
+    }
+    
+    boolean passedReverse(StringBuilder beforeM, StringBuilder afterM, Set<String> route){
+        StringBuilder temp = new StringBuilder(afterM);
+        temp.append(beforeM);
+        return route.contains(temp.toString());
+    }
 }
