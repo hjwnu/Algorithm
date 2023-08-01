215fc1c5802785711071f3bb67be6f7b14907955
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-08-01 22:44:21 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
+        int ans = 1; // 0->n 과 n->0으로 가는 횟수는 동일하다. 아래 반복문은 항상 1에서 멈추므로, 0->1만큼의 이동횟수를 추가해 시작한다.
         while(n>1){
             if(n%2==0){n /= 2;}
             else { n --; ans++;}
         }
-        return ans+1; // 위 반복문은 항상 1에서 멈추므로, 1만큼 이동한다.
+        return ans; 
     }
 }
