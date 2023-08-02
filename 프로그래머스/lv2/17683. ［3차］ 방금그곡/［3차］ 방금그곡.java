dc9b32d2259f8b5aeafc2fbb8f6b9425d4745ce3
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-08-02 14:45:08 +0900

    [level 2] Title: [3차] 방금그곡, Time: 13.55 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,42 +1,59 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
-    public String solution(String m, String[] musicinfos) {
-        m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("A#", "a").replace("G#", "g");
-        HashMap<String, Object> found = new HashMap<>();
-        found.put("time", 0);
-        found.put("name", "(None)");
+     public static String solution(String m, String[] musicinfos) {
+        m = semiNote(m); // 네오가 기억하는 멜로디
 
-        for (String data : musicinfos) {
-            String[] splitData = data.split(",");
-            String start = splitData[0];
-            String end = splitData[1];
-            String name = splitData[2];
-            String song = splitData[3];
+        Map<String, Object> ans = new HashMap<>();
+        ans.put("time",0);
+        ans.put("name","(None)");
 
-            String[] startTokens = start.split(":");
-            int startHour = Integer.parseInt(startTokens[0]);
-            int startMin = Integer.parseInt(startTokens[1]);
+        for(int i =0 ; i < musicinfos.length;i++){
+            String[] tmp = musicinfos[i].split(",");
+            
+            String song = semiNote(tmp[3]); // 곡의 코드 (반음처리)
+            int musicLen = song.length(); // 곡의 재생시간
 
-            String[] endTokens = end.split(":");
-            int endHour = Integer.parseInt(endTokens[0]);
-            int endMin = Integer.parseInt(endTokens[1]);
+            int playT = playTime(tmp); // 라디오에서 재생된 시간
+            String realP = realPlay(song, playT, musicLen); // 라디오에서 재생된 코드 (반음처리)
 
-            int length = (endHour - startHour) * 60 + (endMin - startMin);
-
-            song = song.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("A#", "a").replace("G#", "g");
-            StringBuilder repeatedSong = new StringBuilder();
-            for (int i = 0; i < length / song.length() + 1; i++) {
-                repeatedSong.append(song);
-            }
-            song = repeatedSong.substring(0, length);
-
-            if (song.contains(m) && (int)found.get("time") < length) {
-                found.put("time", length);
-                found.put("name", name);
+            if(realP.contains(m)&& (int)ans.get("time") < playT){
+                ans.put("time",playT);
+                ans.put("name",tmp[2]);
             }
         }
 
-        return String.valueOf(found.get("name"));
+        return String.valueOf(ans.get("name"));
+    }
+
+    public static String semiNote(String note){
+        note = note.replace("C#","c")
+                .replace("D#","d")
+                .replace("F#","f")
+                .replace("G#","g")
+                .replace("A#","a");
+
+        return note;
+    }
+
+    public static String realPlay(String song, int playT, int musicLen){
+        StringBuilder realPlay= new StringBuilder(song);
+        realPlay.append(song.repeat(playT/musicLen));
+        realPlay.setLength(playT);
+        return realPlay.toString();
+    }
+
+    public static int playTime(String[] tmp){
+        int startTime = changeToTime(tmp[0]);
+        int endTime = changeToTime(tmp[1]);
+        
+        return (endTime) - (startTime);
+    }
+    
+    public static int changeToTime(String time){
+        String[] T = time.split(":");
+        int hour = Integer.parseInt(T[0])*60;
+        int min = Integer.parseInt(T[1]);
+        return hour+min;
     }
 }
