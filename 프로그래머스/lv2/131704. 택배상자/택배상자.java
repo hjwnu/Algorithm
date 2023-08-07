da03b53f5c625f532221761fba67679e894b99f0
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-08-07 14:11:11 +0900

    [level 2] Title: 택배상자, Time: 52.87 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -4,12 +4,12 @@ class Solution {
         Stack<Integer> sub = new Stack<>();
         int inTheTruck =0;
         int idx =0;
-        for(int box =1 ; box <= order.length;box++){
-            if(order[idx]!=box) { sub.push(box); continue; }
+        for(int box =1 ; box <= order.length;box++){ // 반복문 자체가 메인 컨테이너 박스.
+            if(order[idx]!=box) { sub.push(box); continue; } // 맨 앞 박스를 못 넣으면, 보조 컨테이너로 보내고 다음 박스.
+            inTheTruck++; idx++; // 같으면, 트럭에 싣고(inTheTruck++) 다음에 실어야 할 박스로(idx++)
             
-            inTheTruck++; idx++;
-            
-            while(!sub.isEmpty()&&sub.peek()==order[idx]) { sub.pop(); inTheTruck++; idx++; }
+            // 보조 컨테이너에 그 다음 박스도 바로 실어야 할 수 있으므로 while
+            while(!sub.isEmpty()&&sub.peek()==order[idx]) { sub.pop(); inTheTruck++; idx++; }   // 보조 컨테이너 맨 앞이 다음에 실을 박스라면, 트럭에 싣고 다음 박스로. 
 
         }
             
