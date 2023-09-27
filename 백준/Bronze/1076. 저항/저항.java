499734bf676654372473ef940b5dd3d60cd6a836
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-09-28 02:12:58 +0900

    [Bronze II] Title: 저항, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -2,20 +2,19 @@ import java.util.*;
 import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException{
-        String[] color = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-        Map<String, long[]> word = new HashMap<>();
-        for(int i =0; i < 10;i++){
-            long[] tmp = new long[2];
-            tmp[0] = i; tmp[1] = (int)Math.pow(10,i);
-            word.put(color[i],tmp);
-        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
-        while(true){
-            answer.append(word.get(br.readLine())[0]);
-            if(answer.length()==2){break;}
+        String word = br.readLine();
+
+        int[] alphabet = new int[26];
+        for(int i =0; i < word.length();i++){
+            alphabet[word.charAt(i)-'a']++;
         }
-        long temp = Long.parseLong(answer.toString());
-        System.out.println(temp*word.get(br.readLine())[1]);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0 ; i < alphabet.length;i++){
+            sb.append(alphabet[i]).append(" ");
+        }
+        sb.setLength(sb.length()-1);
+        System.out.print(sb.toString());
+        br.close();
     }
 }
