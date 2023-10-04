380b8f6e1198611481818f067f27f5b008b9e8d1
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-10-04 22:08:38 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 340 ms, Memory: 47336 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -15,10 +15,10 @@ public class Main{
     public static String recursion(String s, int l, int r, int recursiveCount){
         StringBuilder sb = new StringBuilder();
         if(l >= r) {
-            sb.append("1"+" "+recursiveCount);
+            sb.append("1").append(" ").append(recursiveCount);
             return sb.toString();
         } else if(s.charAt(l) != s.charAt(r)) {
-            sb.append("0"+" "+recursiveCount);
+            sb.append("0").append(" ").append(recursiveCount);
             return sb.toString();
         } else return recursion(s, l+1, r-1,recursiveCount+1);
     }
