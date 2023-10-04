1faa281b55ed691905698f3e04a772a08ae1ade7
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-10-04 22:02:24 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 376 ms, Memory: 47920 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+        int count =0;
+        while(count < total){
+            System.out.println(isPalindrome(br.readLine()));
+            count++;
+        }
+    }
+    
+    public static String recursion(String s, int l, int r, int recursiveCount){
+        StringBuilder sb = new StringBuilder();
+        if(l >= r) {
+            sb.append("1"+" "+recursiveCount);
+            return sb.toString();
+        } else if(s.charAt(l) != s.charAt(r)) {
+            sb.append("0"+" "+recursiveCount);
+            return sb.toString();
+        } else return recursion(s, l+1, r-1,recursiveCount+1);
+    }
+    public static String isPalindrome(String s){
+        return recursion(s, 0, s.length()-1,1);
+    }
+}
