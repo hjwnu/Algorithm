f6a25e6a69689438f65aba1bbc2ec18dde719b41
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-10-07 15:49:32 +0900

    [level 2] Title: 모음 사전, Time: 15.40 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -6,19 +6,19 @@ class Solution {
     
     public int solution(String word) {
         for(int i =0 ; i < vowel.length;i++){
-            addWord(dict, vowel[i], word); // 시작 문자 지정
+            addWord(vowel[i], word); // 시작 문자 지정
         }    
         return dict.indexOf(word)+1;
     }
     
-    private void addWord(List<String> dict, String str, String word){
+    private void addWord(String str, String word){
         if(dict.contains(word)){return;} // 목표 문자열이 사전에 들어왔다면 종료
         if(str.length() > vowel.length) {return;} // 더해진 문자열이 5글자를 넘었다면 종료
         
         if(!dict.contains(str)) dict.add(str); // 사전에 없는 문자면 추가
     
         for(int i=0; i<vowel.length; i++){
-            addWord(dict, str+vowel[i], word); // 기존 문자열에 한글자씩 순회하며 추가
+            addWord(str+vowel[i], word); // 기존 문자열에 한글자씩 순회하며 추가
         }
     }
 }
