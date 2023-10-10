f11735848597e1a8178e6327c569bed588b293e0
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-10-11 03:36:04 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i =0; i <5;i++){
+            int num = sc.nextInt();
+            sum += num*num;
+        }
+        System.out.println(sum%10);
+    }
+}
