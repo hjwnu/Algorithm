48dc37d3ee69d7fa244d8d75e6939db22f136f49
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-10-26 12:15:07 +0900

    [Gold V] Title: AC, Time: 860 ms, Memory: 103596 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,69 +1,54 @@
-import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Deque;
-public class Main{
-    public static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int cnt =1;
-        while(cnt <=T){
-
-            String a = sc.next();
-            int len = sc.nextInt();
-            String b = sc.next();
-            a = a.replace("RR","");
-            b = b.substring(1, b.length() - 1);
-            String[] tmp = b.split(",");
-            Deque<Integer> q = new LinkedList<>();
-            for(int i =0 ; i < len;i++){
-                q.add(Integer.parseInt(tmp[i]));
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+        for (int i = 0; i < total; i++) {
+            String command = br.readLine().replace("RR", "");
+            int length = Integer.parseInt(br.readLine());
+            StringBuilder tempArrString = new StringBuilder(br.readLine());
+            tempArrString.setLength(tempArrString.length() - 1);
+            tempArrString.deleteCharAt(0);
+            String[] tempArr = tempArrString.toString().split(",");
+            Deque<Integer> deq = new LinkedList<>();
+            for (int j = 0; j < length; j++) {
+                deq.add(Integer.parseInt(tempArr[j]));
             }
-            int error =0;
-            boolean reverse =false;
-            for(char ch : a.toCharArray()){
-                if(ch=='R'){reverse = !reverse;}
-                else{
-                    if(!reverse) {
-                        if (!q.isEmpty()) {q.pollFirst();}
-                        else {error = -1; break;}
-                    }
-                    else{
-                        if (!q.isEmpty()) {q.pollLast();}
-                        else {error = -1; break;}
-                    }
+            boolean error = false;
+            boolean reverse = false;
+            loop1:
+            for (char ch : command.toCharArray()) {
+                switch (ch) {
+                    case 'R' : reverse = !reverse; break;
+                    case 'D' : 
+                        if(deq.isEmpty()){error = true; break loop1;}
+                        else {
+                            if(reverse){ deq.pollLast(); break;}
+                            else{ deq.pollFirst(); break;}
+                        }
                 }
             }
-
-            if (error == -1) {System.out.println("error");}
-            else {
-                    printAns(q,reverse);
-                    System.out.println(sb);}
-            cnt++;
+            if(error){ System.out.println("error");}
+            else{printAns(deq, reverse);}
+        }
+    }
+    public static void printAns(Deque<Integer> deq, boolean reverse) {
+            StringBuilder ans = new StringBuilder("[");
+            if (deq.size() > 0) {
+                if(reverse){
+                    ans.append(deq.pollLast());
+                    while (!deq.isEmpty()) {
+                        ans.append(",").append(deq.pollLast());
+                    }
+                } else{
+                    ans.append(deq.pollFirst());
+                    while (!deq.isEmpty()) {
+                        ans.append(",").append(deq.pollFirst());
+                }
             }
         }
-    
-    public static void printAns(Deque<Integer> q, boolean reverse) {
-		sb = new StringBuilder();
-		sb.append('[');
-		
-		if(q.size() > 0) {
-			
-			if(!reverse) {
-				
-				sb.append(q.pollFirst());
-				while(!q.isEmpty()) {
-					sb.append(',').append(q.pollFirst());
-				}
-			}
-			else {	
-				sb.append(q.pollLast());	
-				while(!q.isEmpty()) {
-					sb.append(',').append(q.pollLast());
-				}
-			}
-		}
-		
-		sb.append(']');	
-	}
+        ans.append("]");
+        System.out.println(ans);
+    }
 }
