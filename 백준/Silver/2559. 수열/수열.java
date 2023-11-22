5733d1fb266c8191546b683b3023d8fad7026e30
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-11-22 10:46:34 +0900

    [Silver III] Title: 수열, Time: 272 ms, Memory: 25724 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -6,16 +6,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] seq = inputToIntArr(br.readLine());
         int[] tempo = inputToIntArr(br.readLine());
-        List<Integer> maxSum = new ArrayList<>();
-        for(int i =0; i < seq[0]-seq[1]+1;i++){
-            int max = 0;
-            for(int j =i ; j < i+seq[1];j++){
-                max += tempo[j];
-            }
-            maxSum.add(max);
+        int max = 0;
+        for(int i =0; i < seq[1];i++){
+            max += tempo[i];
         }
-        Collections.sort(maxSum);
-        System.out.println(maxSum.get(maxSum.size()-1));
+        int next = max;
+        for(int i =0 ; i < seq[0]-seq[1];i++){
+            next += tempo[i+seq[1]] - tempo[i];
+            max = Math.max(next,max);
+        }
+        
+        System.out.println(max);
     }
     
     public static int[] inputToIntArr(String input){
