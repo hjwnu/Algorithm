8d19d08834a828fafcee363b03ef31f5928a652c
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-11-24 10:52:14 +0900

    [Silver IV] Title: 듣보잡, Time: 508 ms, Memory: 28116 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] total = inputToIntArr(br.readLine());
+        int deud = total[0]; int bo = total[1];
+        Map<String, Integer> deudbos = new TreeMap<>();
+        int cnt =0;
+        for(int i =0 ; i < deud+bo; i++){
+            String input = br.readLine();
+            if(deudbos.containsKey(input)){
+                cnt++;
+                deudbos.put(input, deudbos.get(input)+1);
+            } else {
+            deudbos.put(input, 1);
+            }
+        }
+        System.out.println(cnt);
+        for(Map.Entry<String, Integer> entry : deudbos.entrySet()){
+            if(entry.getValue()==2){
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+    public static int[] inputToIntArr(String input){
+        String[] temp = input.split(" ");
+        int[] arr = new int[temp.length];
+        for(int i =0; i < temp.length;i++){
+            arr[i] = Integer.parseInt(temp[i]);
+        }
+
+        return arr;
+    }
+}
