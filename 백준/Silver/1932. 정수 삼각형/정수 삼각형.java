5bc161a525fbadd9e276eeaef66a9774979853eb
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-12-11 11:18:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 324 ms, Memory: 30516 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] triangle = new int[n][n];
+        for(int i = 0 ; i < n ; i++){
+            triangle[i] = toIntArr(br.readLine(),n);
+        }
+        loop1:
+        for(int i =1; i < n;i++){
+            for(int j = 0; j <= i;j++){
+                if(j==0){triangle[i][j] += triangle[i-1][j]; }
+                else {
+                    triangle[i][j] += Math.max(triangle[i - 1][j-1], triangle[i - 1][j]);
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i =0 ; i < n ; i ++){
+            max = Math.max(triangle[n-1][i],max);
+        }
+        System.out.println(max);
+    }
+    public static int[] toIntArr(String input, int n){
+        String[] temp = input.split(" ");
+        int[] arr = new int[n];
+        for(int i =0; i < temp.length;i++){
+            arr[i] = Integer.parseInt(temp[i]);
+        }
+
+        return arr;
+    }
+}
