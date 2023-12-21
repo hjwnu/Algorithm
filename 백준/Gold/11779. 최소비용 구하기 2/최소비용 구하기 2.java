7c1d7ff350ad7780fb8fcc3dd42ce752baab3aa8
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-12-21 14:45:41 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 584 ms, Memory: 52260 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,90 +1,78 @@
-import java.io.*;
 import java.util.*;
-
-// BOJ / 최소비용 구하기2 / G3 / 45분
-// https://www.acmicpc.net/problem/11779
-public class Main {
-	static class City implements Comparable<City> {
-		int to;
-		int weight;
-
-		public City(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(City o) { // 비용 오름차순 정렬
-			return this.weight - o.weight;
-		}
-	}
-
-	static int N, M;
-	static int d[], preCity[];
-	static int start, end;
-	static List<ArrayList<City>> graph;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-
-		d = new int[N + 1]; // 해당 도시까지 가는데의 최소 비용
-		preCity = new int[N + 1]; // 이전도시
-		Arrays.fill(d, Integer.MAX_VALUE);
-
-		graph = new ArrayList<>();
-		for (int i = 0; i < N + 1; i++)
-			graph.add(new ArrayList<City>());
-
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			graph.get(from).add(new City(to, weight));
-
-		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
-
-		dijkstra(start);
-		System.out.println(d[end]); // 최단 거리
-		// 경로 역추적
-		int cnt = 0;
-		Stack<Integer> stack = new Stack<>();
-		stack.push(end);
-		while (preCity[end] != 0) {
-			cnt += 1;
-			stack.push(preCity[end]);
-			end = preCity[end];
-		}
-		System.out.println(cnt + 1);
-		while (!stack.isEmpty()) {
-			System.out.print(stack.pop() + " ");
-		}
-
-	}
-
-	private static void dijkstra(int start) {
-		PriorityQueue<City> pq = new PriorityQueue<City>();
-		pq.add(new City(start, 0));
-		d[start] = 0;
-
-		while (!pq.isEmpty()) {
-			City curCity = pq.poll();
-			int cur = curCity.to;
-			if (d[cur] < curCity.weight)
-				continue;
-			for (City next : graph.get(cur)) {
-				if (d[next.to] > d[cur] + next.weight) { // 최단거리 cost 업데이트
-					d[next.to] = d[cur] + next.weight;
-					preCity[next.to] = cur; // 이전마을 기록
-					pq.offer(new City(next.to, d[next.to]));
-				}
-			}
-		}
-
-	}
+import java.io.*;
+public class Main{
+    static List<List<Node>> list = new ArrayList<>();
+    static int start;
+    static int cities;
+    static int[] dist, prev;
+    public static class Node implements Comparable<Node>{
+        private int num;
+        private int cost;
+        public Node(int num, int cost){
+            this.num = num;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Node n){
+            return Integer.compare(this.cost, n.cost);
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        cities = Integer.parseInt(br.readLine());
+        int buses = Integer.parseInt(br.readLine());
+        dist = new int[cities+1];
+        prev = new int[cities+1];
+        for(int i = 0 ; i <= cities;i++){
+            list.add(new ArrayList<>());
+            dist[i] = Integer.MAX_VALUE;
+        }
+        for(int i = 0; i < buses; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list.get(start).add(new Node(end,cost));
+        }
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        findMinCost(start,end);
+        reverseTrace(end);
+    }
+    private static void reverseTrace(int end){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(end);
+        while(prev[end]!=0){
+            stack.push(prev[end]);
+            end = prev[end];
+        }
+        System.out.println(stack.size());
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop()+" ");
+        }
+        sb.setLength(sb.length()-1);
+        System.out.println(sb);
+    }
+    private static void findMinCost(int start, int end){
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        dist[start] = 0;
+        q.add(new Node(start,0));
+        while(!q.isEmpty()){
+            Node n = q.poll();
+            int startNode = n.num;
+            if(dist[startNode] >= n.cost){
+                for(Node node : list.get(startNode)){
+                    if(dist[node.num] > (dist[startNode]+node.cost)){
+                        dist[node.num] = dist[startNode]+node.cost;
+                        prev[node.num] = startNode;
+                        q.add(new Node(node.num,dist[node.num]));
+                    }
+                }
+            }
+        }
+        System.out.println(dist[end]);
+    }
 }
