48f12d2a75e636c91f2a125f2598eecccc7bc464
hjwnu <130176882+hjwnu@users.noreply.github.com>
2024-01-31 17:07:20 +0900

    [level 1] Title: 예산, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,8 +1,8 @@
 def solution(d, budget):
-    count = 0
+    origin = len(d)
     for i in sorted(d):
         if budget - i < 0 :
             break
         budget -= i
-        count += 1
-    return count
+        d.remove(i)
+    return origin - len(d)
