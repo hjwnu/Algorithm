45f7706f53d9df567ed2e9c4790be35582bf8f4c
hjwnu <130176882+hjwnu@users.noreply.github.com>
2024-01-31 14:36:54 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,3 +1,3 @@
 from math import *
 def solution(left, right):
-    return sum([int(i) if sqrt(i)%1>0 == 0 else -1*int(i) for i in range(left,right+1)])
+    return sum([i if sqrt(i)%1>0 == 0 else -i for i in range(left,right+1)])
