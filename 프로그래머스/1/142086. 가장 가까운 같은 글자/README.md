f78fb8927ebcdf797c552495ab3bd509c1d03c2d
hjwnu <130176882+hjwnu@users.noreply.github.com>
2024-02-04 11:29:03 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.84 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=python3) 
 
 ### 성능 요약
 
-메모리: 11 MB, 시간: 1.85 ms
+메모리: 11.1 MB, 시간: 1.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 11:28:54
+2024년 2월 0일 11:29:4
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -4,9 +4,9 @@ def solution(s):
     for i in range(len(s)):
         if s[i] not in dictionary:
             dictionary[s[i]] = i
-            answer.append(-1)  # 리스트에 값을 추가합니다.
+            answer.append(-1) 
         else:
             index = dictionary[s[i]]
-            answer.append(i - index)  # 현재 인덱스와 마지막 출현 인덱스의 차이를 계산하여 추가합니다.
-            dictionary[s[i]] = i  # 문자의 마지막 출현 위치를 업데이트합니다.
+            answer.append(i - index)
+            dictionary[s[i]] = i
     return answer
