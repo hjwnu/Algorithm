7c26dd9931f243824ef7c9bf34d7f0eb6c4aa755
hjwnu <130176882+hjwnu@users.noreply.github.com>
2024-05-11 17:48:42 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < n; j++){
+                if(!visited[j]) {
+                    bfs(computers, j);
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public void bfs(int[][] computers, int from){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(from);
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            visited[cur] = true;
+            for(int i = 0; i < computers.length; i++){
+                if(computers[cur][i] == 1 && !visited[i]) q.add(i);
+            }
+        }
+    }
+}
