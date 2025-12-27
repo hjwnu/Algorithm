a231b974cabcd55f370cade29b23fa2a5b6cbd49
hjwnu <130176882+hjwnu@users.noreply.github.com>
2025-12-27 12:16:27 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        if (A.equals(B)) return 0;
-
-        String cur = A;
-        for (int i = 1; i <= A.length(); i++) {
-            cur = cur.charAt(cur.length() - 1) + cur.substring(0, cur.length() - 1);
-            if (cur.equals(B)) return i;
-        }
-        return -1;
+        String tempB = B.repeat(2);
+        return tempB.indexOf(A);
     }
 }
