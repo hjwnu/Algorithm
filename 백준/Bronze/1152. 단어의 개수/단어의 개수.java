2d3571008a8ca734edd42744364554992495e400
hjwnu <130176882+hjwnu@users.noreply.github.com>
2025-12-31 13:26:55 +0900

    [Bronze II] Title: 단어의 개수, Time: 252 ms, Memory: 20148 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,13 +1,8 @@
 import java.util.*;
+import java.io.*;
 public class Main{
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        String sequence = in.nextLine();
-        String[] arr = sequence.trim().split(" ");
-        int cnt=0;
-        for(int i =0; i < arr.length;i++){
-            if(arr[i].matches(".*[A-Za-z].*")){cnt++;}
-        }
-        System.out.println(cnt);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(new StringTokenizer(br.readLine()).countTokens());
     }
 }
