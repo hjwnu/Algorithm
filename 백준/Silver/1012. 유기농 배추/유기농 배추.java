5fbe4fe8efb77e7d45337b97f4e3ba44d57ba204
hjwnu <130176882+hjwnu@users.noreply.github.com>
2026-01-02 19:09:06 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 17180 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,67 +2,58 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-   static int[][] matrix;
-    static int[][] direction = {{1,0},{0,1},{-1,0},{0,-1}};
-    static int M,N;
-    static boolean[][] visited;
-    static int count;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        for(int i = 0; i < tc; i++){
-            count = 0;
-            //getMatrix(br);
-            StringTokenizer st;
-            st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            matrix = new int[N][M];
-            visited = new boolean[N][M];
-            for(int j = 0; j < K; j++){
-                st = new StringTokenizer(br.readLine());
-                int m = Integer.parseInt(st.nextToken());
-                int n = Integer.parseInt(st.nextToken());
-                matrix[n][m] = 1;
-            }
-            for(int x = 0; x < N; x++){
-                for(int y = 0; y <M; y++){
-                    if(matrix[x][y] == 1
-                      && !visited[x][y]) bfs(x,y);
-                }
-            }
-            
-            System.out.println(count);
-        }
-    }
+	static boolean[][] matrix;
+	static boolean[][] visited;
+	static int answer;
+	static int[][] direction = {{-1,0},{1,0},{0,1},{0,-1}};
+	static int row, col;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < n; i++){
+			String[] temp = br.readLine().split(" ");
+			row = Integer.parseInt(temp[0]);
+			col = Integer.parseInt(temp[1]);
+			matrix = new boolean[col][row];
+			visited = new boolean[col][row];
+			for(int j = 0; j < Integer.parseInt(temp[2]);j++){
+				String[] tmp = br.readLine().split(" ");
+				matrix[Integer.parseInt(tmp[1])][Integer.parseInt(tmp[0])] = true;
+			}
+			answer = 0;
+			for(int j = 0 ; j < matrix.length;j++){
+				for(int k = 0; k < matrix[j].length;k++){
+					if(matrix[j][k] && !visited[j][k]) bfs(j,k);
+				}
+			}
+			System.out.println(answer);
 
-    public static void bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            
-            if(visited[curX][curY]) continue;
-            visited[curX][curY] = true;
-            
-            for(int[] d : direction){
-                int moveX = curX + d[0];
-                int moveY = curY + d[1];
-                if(moveX >= 0 && moveX < N
-                        && moveY >= 0 && moveY < M
-                        && matrix[moveX][moveY] == 1
-                        && !visited[moveX][moveY]){
-                    q.add(new int[]{moveX,moveY});
-                }
-            }
-        }
-        count++;
-    }
+		}
+	}
 
-    public static void getMatrix(BufferedReader br) throws IOException {
-        
-    }
+	public static void bfs(int x, int y){
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[]{x,y});
+		visited[x][y] = true;
+
+		while(!q.isEmpty()){
+			int[] cur = q.poll();
+			int curX = cur[0];
+			int curY = cur[1];
+			for(int[] d : direction){
+				int moveX = curX + d[0];
+				int moveY = curY + d[1];
+
+				if((moveX < 0 || moveX >= col)
+						|| (moveY < 0 || moveY >= row)
+						|| !matrix[moveX][moveY]
+						|| visited[moveX][moveY]) continue;
+
+				q.add(new int[]{moveX, moveY});
+				visited[moveX][moveY] = true;
+			}
+		}
+
+		answer++;
+	}
 }
