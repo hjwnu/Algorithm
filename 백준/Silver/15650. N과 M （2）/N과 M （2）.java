ff07e7f178f3745eef34cb2009658fb2d5a5b645
hjwnu <130176882+hjwnu@users.noreply.github.com>
2026-01-04 20:08:45 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,37 +4,33 @@ import java.util.*;
 class Main {
 	static int total;
 	static int target;
-	static boolean[] used;
+	static int[] seq;
 	static List<String> list = new ArrayList<>();
+
 	public static void main (String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		total = Integer.parseInt(input[0]);
 		target = Integer.parseInt(input[1]);
-		used = new boolean[total];
-		backTrack(0);
+		seq = new int[target+1];
+		backTrack(0, 1);
 
-		for(String sb : list) System.out.println(sb.trim());
+		for (String sb : list) System.out.println(sb.trim());
 	}
 
-	static void backTrack(int depth){
-		if(depth == target){
+	static void backTrack (int depth, int start) {
+		if (depth == target) {
 			StringBuilder sb = new StringBuilder();
-			for(int i = 0; i < total; i++){
-				if(used[i]){
-					sb.append(i + 1).append(" ");
-				}
+			for (int i = 0; i < target; i++) {
+				sb.append(seq[i]).append(" ");
 			}
-			String s = sb.toString();
-			if(!list.contains(s)) list.add(s);
+			sb.append('\n');
+			list.add(sb.toString());
 			return;
 		}
-		for(int i = 0; i < total; i++){
-			if(!used[i]){
-				used[i] = true;
-				backTrack(depth + 1);
-				used[i] = false;
-			}
+		for (int i = start; i <= total; i++) {
+			seq[depth] = i;
+			backTrack(depth + 1, i+1);
 		}
 	}
 }
